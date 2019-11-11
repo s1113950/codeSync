ddbf834a93885b30b38cd49fae3822b246a4fee9
Steven Robertson <srtrumpetaggie@gmail.com>
2019-11-10 22:12:49 -0800

    converted to pip package; offline so can't test yet

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import versioneer
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='codesync',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description='Syncs code to machines on code changes',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Steven Robertson',
+      url='https://github.com/s1113950/codeSync.git',
+      packages=find_packages(exclude=["tests"]),
+      install_requires=[
+          "configparser==3.5.0",
+          "watchdog==0.8.3"
+      ],
+      entry_points={
+          'console_scripts': ['codesync=codesync.watcher:main']
+      },
+      package_data={
+      },
+      include_package_data=True,
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Environment :: Console',
+                   'Intended Audience :: End Users/Desktop',
+                   'Intended Audience :: Developers',
+                   'Operating System :: POSIX',
+                   'Operating System :: MacOS :: MacOS X',
+                   'Programming Language :: Python :: 3'])
